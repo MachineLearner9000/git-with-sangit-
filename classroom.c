14add3481bf63b4541955879e25d7416186273c4
MachineLearner9000 <Ankushsedhai72@gmail.com>
2024-03-02 12:20:44 +0545

    messsage

---
+++ classroom.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+ float C ;
+ float F;
+ printf("Enter the value of C : ");
+ scanf("%f",&C);
+ F=(9.0/5)*C+32;
+ printf("\nThe %.2f equivalent in Fahrenheit is %.2f ",C,F);
+ 
+}
